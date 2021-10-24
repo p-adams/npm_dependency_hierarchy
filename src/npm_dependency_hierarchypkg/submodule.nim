107de5ca7eee6fcd98ad278e515b9d37ac509c79
Paul Adams <ghusiyy@gmail.com>
2021-10-24 01:44:22 -0700

    add test package.json file

--- src/npm_dependency_hierarchypkg/submodule.nim
+++ src/npm_dependency_hierarchypkg/submodule.nim
@@ -7,8 +7,10 @@ type
 
 
 proc getNpmDependencies*(): int =
+    let file = readFile("package.json")
     let dependencies: seq[Dependency] = @[]
     let dep = Dependency(package_name: "svelte", version_range: "^3.34.0")
     
+    echo file
     result = 1
 
